b1d2727e19603a51690cfe7a9469fd482fdd1998
Rohit Dhan <rohitr@mapmyindia.com>
2025-02-17 14:20:29 +0530

    review and contact page chage

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-reviews',
@@ -47,13 +47,55 @@ export class ReviewsComponent {
     },
     // Add more reviews as required
   ];
-  isHovered = false;
+  paginatedReviews: any[] = [];
+  currentIndex = 0;
+  reviewsPerPage = 4; // Change dynamically based on screen size
 
-  pauseScroll() {
-    this.isHovered = true;
+  constructor() {
+    // this.updateVisibleReviews();
+    this.setResponsiveReviewCount();
   }
 
-  resumeScroll() {
-    this.isHovered = false;
+  nextReview() {
+    this.currentIndex += this.reviewsPerPage;
+    if (this.currentIndex >= this.reviews.length) {
+      this.currentIndex = 0; // Loop back to start
+    }
+    this.updateVisibleReviews();
+  }
+
+  prevReview() {
+    this.currentIndex -= this.reviewsPerPage;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.reviews.length - this.reviewsPerPage;
+    }
+    this.updateVisibleReviews();
+  }
+
+  updateVisibleReviews() {
+    this.paginatedReviews = this.reviews.slice(this.currentIndex, this.currentIndex + this.reviewsPerPage);
+  }
+
+  setResponsiveReviewCount() {
+    const screenWidth = window.innerWidth;
+    console.log(screenWidth);
+    
+    if (screenWidth < 400) {
+      this.reviewsPerPage = 1;
+    } else if (screenWidth > 400 && screenWidth <= 760) {
+      this.reviewsPerPage = 2;
+    }else if (screenWidth > 760 && screenWidth <= 1024) {
+      this.reviewsPerPage = 3;
+    }
+     else {
+      this.reviewsPerPage = 3;
+    }
+    this.updateVisibleReviews();
+  }
+
+  // Listen to window resize and adjust number of reviews shown
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.setResponsiveReviewCount();
   }
 }
